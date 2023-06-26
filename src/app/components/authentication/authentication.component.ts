db66bce3c1969cfc1a667bb10f5061d7e13bbd03
Stéphane Menut <smenut@dawan.fr>
2023-06-26 14:16:17 +0200

    authentication redirection

--- src/app/components/authentication/authentication.component.ts
+++ src/app/components/authentication/authentication.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AuthenticatorService } from "../../security/authenticator.service";
 
 @Component({
@@ -6,14 +6,25 @@ import { AuthenticatorService } from "../../security/authenticator.service";
   templateUrl: './authentication.component.html',
   styleUrls: ['./authentication.component.css']
 })
-export class AuthenticationComponent implements OnInit {
+export class AuthenticationComponent implements OnInit, AfterViewInit {
 
   state: boolean = false
+
+  // { static: false } -> les composants enfants ne sont pas accessibles dans le ngOnInit
+  // { static: true } -> les composants enfants sont accessibles dans le ngOnInit
+  @ViewChild('username', { static: false }) inputUsername!: ElementRef;
+
+  // derrière une variable le ? signifie que la variable peut être undefined
+  // derrière une variable le ! signifie que la variable doit être du type et non null
 
   constructor(private authenticator: AuthenticatorService) { }
 
   ngOnInit() {
     this.state = this.authenticator.isAuthenticate();
+  }
+
+  ngAfterViewInit() {
+    this.inputUsername.nativeElement.focus();
   }
 
   public authenticate(): void {
--- src/app/guards/secure-route.guard.ts
+++ src/app/guards/secure-route.guard.ts
@@ -1,10 +1,15 @@
-import { CanActivateFn } from '@angular/router';
+import { CanActivateFn, Router } from '@angular/router';
 import { inject } from "@angular/core";
 import { AuthenticatorService } from "../security/authenticator.service";
 
 export const secureRouteGuard: CanActivateFn = (route, state) => {
 
   const authenticator = inject(AuthenticatorService);
+  const router: Router = inject(Router);
+
+  if( !authenticator.isAuthenticate() ) {
+    return router.parseUrl('/login');
+  }
 
   return authenticator.isAuthenticate();
 };
